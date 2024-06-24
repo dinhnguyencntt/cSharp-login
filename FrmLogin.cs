b1eb006620cb3efe8520db5365262769bf0a6795
dinhnguyencntt <dn1994099@gmail.com>
2024-06-24 09:23:09 +0700

    Add project files.

---
+++ FrmLogin.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace cSharp_login
+{
+    public partial class FrmLogin : Form
+    {
+        public FrmLogin()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text =="nguyen"&& textBox2.Text == "123")
+            {
+                this.Hide();
+                FrmMain frmMain = new FrmMain();
+                frmMain.ShowDialog();
+                this.Close();
+
+            }
+            else
+            {
+                MessageBox.Show("sai tên đăng nhặp hoặc mặt khẩu","lỗi",MessageBoxButtons.OK,MessageBoxIcon.Error);
+            }
+        }
+    }
+}
